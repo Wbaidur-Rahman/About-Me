f09aba4ac60f00ae3ea823b855fa5fecd89ffa9d
Wbaidur Rahman <wbaidurrahman2000@gmail.com>
2022-06-14 18:35:13 +0600

    Button and text field added

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            addnickname(it)
+        }
+    }
+
+    private fun addnickname(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nickNameTextView = findViewById<TextView>(R.id.nickname)
+
+        nickNameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nickNameTextView.visibility = View.VISIBLE
     }
 }
